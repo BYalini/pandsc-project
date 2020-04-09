32f46618d328fa545aa34cf99a09b714503eb904
BYalini <G00387915@gmit.ie>
2020-04-09 22:25:08 +0100

    seaborn histograms added

--- analysis.py
+++ analysis.py
@@ -4,9 +4,11 @@
 # Four features(variables) were measured for each sample:  the length and the width of the sepals and petals, in centimeters.
 # Ref : https://en.wikipedia.org/wiki/Iris_flower_data_set
 # Ref: redirecting print output to file https://realpython.com/python-print/#printing-to-a-file
+# Ref : https://seaborn.pydata.org/tutorial/distributions.html
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #read Iris.Data csv file into pandas datframe
 col_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
@@ -59,3 +61,16 @@ plt.hist(iris_df['petal_width'])
 plt.xlabel('petal width')
 plt.ylabel('Frequency')
 plt.savefig('petal_width_hist.png')
+
+#histogram of each variable differentiation each species
+fig = sns.FacetGrid(iris_df,hue='species',height=5).map(sns.distplot,'sepal_length', kde=False).add_legend()
+fig.savefig('sepal_length_snshist.png')
+
+fig = sns.FacetGrid(iris_df,hue='species',height=5).map(sns.distplot,'sepal_width', kde=False).add_legend()
+fig.savefig('sepal_width_snshist.png')
+
+fig = sns.FacetGrid(iris_df,hue='species',height=5).map(sns.distplot,'petal_length', kde=False).add_legend()
+fig.savefig('petal_length_snshist.png')
+
+fig = sns.FacetGrid(iris_df,hue='species',height=5).map(sns.distplot,'petal_width', kde=False).add_legend()
+fig.savefig('petal_width_snshist.png')
